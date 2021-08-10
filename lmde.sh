75396789d865b22fcdccd99fd8a6d1f6718aa703
KYLE <85462948+SK386@users.noreply.github.com>
2021-08-10 08:19:15 -0300

    Add files via upload
    Tentativa de script para configurar desktop linux

---
+++ lmde.sh
@@ -0,0 +1,55 @@
+#ATUALIZANDO O SISTEMA
+
+sudo apt update&&sudo apt upgrade
+
+#INSTALANDO PROGRAMAS DE PRODUTIVIDADE
+
+sudo apt install kdenlive&&sudo apt install audacity
+sudo apt update
+sudo apt install simplescreenrecorder&&sudo apt install gimp
+sudo apt install idle&&sudo apt install sublime-text
+sudo apt install wine-stable&&sudo apt install q4wine
+sudo apt install guvcview&&sudo apt install klavaro
+sudo apt install boot-repair&&sudo apt install gparted
+sudo apt install whatsapp&&sudo apt install telegram-desktop
+sudo apt install neofetch
+sudo apt install fonts-crosextra-carlito&& sudo apt install fonts-crosextra-caladea
+sudo apt install python3.9
+sudo apt install ttf-mscorefonts-installer
+sudo apt update&& sudo apt upgrade
+#Instalando programas
+cd "Downloads/programas"
+sudo dpkg -i "discord-0.0.15.deb" &&sudo dpkg -i "teams_1.4.00.7556_amd64.deb" &&sudo dpkg -i "wps-office_11.1.0.10161.XA_amd64.deb"
+cd .. &&cd ..
+sudo apt update
+#BAIXANDO PROGRAMAS FORA DO REPOSITORIO
+
+firefox  "https://drive.google.com" "https://addons.mozilla.org/pt-BR/firefox/addon/adfly-skipper/" "https://addons.mozilla.org/pt-BR/firefox/addon/traduzir-paginas-web/" "https://addons.mozilla.org/pt-BR/firefox/addon/darkreader/"
+
+#CRIANDO DIRETORIOS
+mkdir ".fonts"
+#CRIANDO pastas
+cd "Documentos"/
+mkdir "canal" "sesi" "senai" "programação"
+cd "sesi"/
+mkdir "biologia" "filosofia" "historia" "literatura" "quimica" "ed fisica" 
+
+mkdir "fisica"
+
+mkdir "inglês" "matematica" "redação" "espanhol" "geografia" "jornada" 
+
+mkdir "português" "sociologia"
+
+cd ~
+
+#Mover diretorio
+cd "Downloads"
+mv fontes/* /home/user/.fonts
+mv fotos/* home/user/Documentos
+mv sesi/* home/user/Documentos/sesi
+mv senai/* home/user/Documentos/senai
+mv "papeis de parede"/ home/user/Imagens
+
+cd ~
+sudo apt update&&sudo apt upgrade
+sudo apt autoremove
