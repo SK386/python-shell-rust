fbe007b4c3aaccb07312e7f213fbb40309c8a217
Redson <redson@riseup.net>
2022-03-27 09:45:37 -0300

    Hello World feito!

---
+++ rust/hello_world/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Hello world com String em uma variável.
+    let hello = String::from("Hello, world!");
+    println!("{}", hello);
+
+    // Hello world "normal".
+    println!("Hello, world!");
+}
